8065daf5e96eab1a47f03471ba4db9614afbffc5
mannpatel11 <patelmannf@gmail.com>
2025-11-19 15:55:57 -0500

    reformatted to make everything legible

--- kaldiReadWrite.py
+++ kaldiReadWrite.py
@@ -1,19 +1,21 @@
 import numpy as np
 
+
 def writeKaldiProbabilityMatrix(charProb, i, kaldiFile):
     """
     Converts a numpy character probability matrix 'charProb' into a Kaldi matrix file for language model decoding.
     """
-    kFile = open(kaldiFile,'w');
-    kFile.write(str(i) + ' [\n');
+    kFile = open(kaldiFile, "w")
+    kFile.write(str(i) + " [\n")
     for t in range(charProb.shape[0]):
         for k in range(charProb.shape[1]):
-            kFile.write('%.10g ' % charProb[t,k])
-        kFile.write('\n')
+            kFile.write("%.10g " % charProb[t, k])
+        kFile.write("\n")
 
-    kFile.write(']\n')
+    kFile.write("]\n")
     kFile.close()
-    
+
+
 def readKaldiLatticeFile(fileName, fileType):
     """
     Extract the candidate sentences frmo a Kaldi lattice file.
@@ -21,37 +23,38 @@ def readKaldiLatticeFile(fileName, fileType):
     file = open(fileName, "r")
     allLines = file.readlines()
 
-    sentenceNumber = np.zeros([len(allLines),2])
+    sentenceNumber = np.zeros([len(allLines), 2])
     content = []
-    
+
     for x in range(len(allLines)):
-        splitStr = allLines[x].split(' ')
-        
+        splitStr = allLines[x].split(" ")
+
         num = splitStr[0]
-        num = num.split('-')
-        
-        sentenceNumber[x,0] = int(num[0])
-        sentenceNumber[x,1] = int(num[1])
-        
-        if fileType=='numeric':
+        num = num.split("-")
+
+        sentenceNumber[x, 0] = int(num[0])
+        sentenceNumber[x, 1] = int(num[1])
+
+        if fileType == "numeric":
             content.append(float(splitStr[1]))
-        elif fileType=='string':
+        elif fileType == "string":
             del splitStr[0]
             for x in range(len(splitStr)):
-                if splitStr[x]=='<space>':
-                    splitStr[x] = ' '
-                elif splitStr[x]=='\n':
-                    splitStr[x] = ''
-            
-            content.append(''.join(splitStr))
+                if splitStr[x] == "<space>":
+                    splitStr[x] = " "
+                elif splitStr[x] == "\n":
+                    splitStr[x] = ""
+
+            content.append("".join(splitStr))
         else:
-            print('Wrong file type.')
+            print("Wrong file type.")
             raise ValueError
-            
-    if fileType=='numeric':
+
+    if fileType == "numeric":
         content = np.array(content)
-        
+
     return sentenceNumber, content
+
 
 def readKaldiAliFile(fileName):
     """
@@ -60,24 +63,24 @@ def readKaldiAliFile(fileName):
     file = open(fileName, "r")
     allLines = file.readlines()
 
-    sentenceNumber = np.zeros([len(allLines),2])
+    sentenceNumber = np.zeros([len(allLines), 2])
     content = []
-    
+
     for x in range(len(allLines)):
-        splitStr = allLines[x].split(' ')
-        
+        splitStr = allLines[x].split(" ")
+
         num = splitStr[0]
-        num = num.split('-')
-        
-        sentenceNumber[x,0] = int(num[0])
-        sentenceNumber[x,1] = int(num[1])
-        
-        tmp = np.zeros([len(splitStr)-1])
-        for x in range(1, len(splitStr)-1):
+        num = num.split("-")
+
+        sentenceNumber[x, 0] = int(num[0])
+        sentenceNumber[x, 1] = int(num[1])
+
+        tmp = np.zeros([len(splitStr) - 1])
+        for x in range(1, len(splitStr) - 1):
             tmp[x] = int(splitStr[x])
-            
+
         content.append(tmp)
-        
-    content = np.stack(content,axis=0)
-        
+
+    content = np.stack(content, axis=0)
+
     return sentenceNumber, content
--- parallelBash.py
+++ parallelBash.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import numpy as np
 import os
-    
+
+
 def parallelBash(argList, scriptFile, bashFilePrefix, nProcesses):
     """
     A simple utility for making a series of bash scripts that can launch parallel instances of programs.
-    We use this to launch multiple kaldi decoders at the same time. 
-    
+    We use this to launch multiple kaldi decoders at the same time.
+
     argList is a list of argument dictionaries that will be passed to the program for each run
     scriptFile is a path to the python script to run
     bashFilePrefix is that name of the bash scripts this function will create
@@ -15,39 +16,39 @@ def parallelBash(argList, scriptFile, bashFilePrefix, nProcesses):
     nRuns = len(argList)
     nPerProcess = int(np.ceil(nRuns / nProcesses))
     runIdx = 0
-    
-    #put all tasks belonging to a single process in a bash script that launches them serially
+
+    # put all tasks belonging to a single process in a bash script that launches them serially
     for processIdx in range(nProcesses):
-        fileName = bashFilePrefix + '_' + str(processIdx) + '.sh' 
-        file = open(fileName,'w') 
-        
+        fileName = bashFilePrefix + "_" + str(processIdx) + ".sh"
+        file = open(fileName, "w")
+
         for i in range(nPerProcess):
-            if runIdx>=len(argList):
+            if runIdx >= len(argList):
                 break
-                
+
             commandStr = scriptFile
             args = argList[runIdx]
-            
-            for argName in args:
-                if argName.endswith('_mainArg'):
-                    commandStr = commandStr + ' ' + str(args[argName])
-                else:
-                    commandStr = commandStr + ' --' + argName + ' ' + str(args[argName])
 
-            commandStr = commandStr + ' \n'
+            for argName in args:
+                if argName.endswith("_mainArg"):
+                    commandStr = commandStr + " " + str(args[argName])
+                else:
+                    commandStr = commandStr + " --" + argName + " " + str(args[argName])
+
+            commandStr = commandStr + " \n"
             file.write(commandStr)
-            
+
             runIdx += 1
 
-        file.close()    
+        file.close()
         os.chmod(fileName, 0o777)
 
-    #master script that launches all processes at once
-    fileName = bashFilePrefix + '_master.sh'
-    file = open(fileName,'w') 
+    # master script that launches all processes at once
+    fileName = bashFilePrefix + "_master.sh"
+    file = open(fileName, "w")
     for processIdx in range(nProcesses):
-        commandStr = bashFilePrefix + '_' + str(processIdx) + '.sh & \n'
+        commandStr = bashFilePrefix + "_" + str(processIdx) + ".sh & \n"
         file.write(commandStr)
 
-    file.close()    
+    file.close()
     os.chmod(fileName, 0o777)
